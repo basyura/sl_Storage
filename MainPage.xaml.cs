d378cfcc9e233b8ba5c657d562df8e01eec62f24
basyura <basyura@gmail.com>
2011-07-10 20:57:54 +0900

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.IO.IsolatedStorage;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+namespace sl_Storage
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (IsolatedStorageSettings.ApplicationSettings.Contains("MyMessage"))
+            {
+                textMessage.Text =
+                    IsolatedStorageSettings.ApplicationSettings["MyMessage"] as string;
+            }
+            if (IsolatedStorageSettings.ApplicationSettings.Contains("MyFlag"))
+            {
+                checkFlag.IsChecked = IsolatedStorageSettings.ApplicationSettings["MyFlag"] as bool?;
+            }
+            LoadIcon();
+
+        }
+        private void LoadIcon()
+        {
+            using (var store = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if (!store.FileExists("icon.img")) {
+                    return;
+                }
+                using (IsolatedStorageFileStream stream = store.OpenFile("icon.img" , FileMode.Open)) {
+                    try {
+                        BitmapImage image = new BitmapImage();
+                        image.SetSource(stream);
+                        imageIcon.Source = image;
+
+                    }
+                    catch {
+                        imageIcon.Source = null;
+                    }
+                }
+            }
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e) {
+            IsolatedStorageSettings.ApplicationSettings["MyMessage"] = textMessage.Text;
+            IsolatedStorageSettings.ApplicationSettings["MyFlag"] = checkFlag.IsChecked;
+        }
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            IsolatedStorageSettings.ApplicationSettings.Remove("MyMessage");
+            IsolatedStorageSettings.ApplicationSettings.Remove("MyFlag");
+        }
+        private void btnChoose_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.Filter = "Image files(*.jpg,*.png,)|*.jpg;*.png";
+            if(dlg.ShowDialog() == true) {
+                using (var store = IsolatedStorageFile.GetUserStoreForApplication()) {
+                    if (store.FileExists("icon.img")) {
+                        store.DeleteFile("icon.img");
+                    }
+
+                    FileStream streamSource = dlg.File.OpenRead();
+
+                    using (IsolatedStorageFileStream streamDest = store.CreateFile("icon.img")) {
+                        byte[] buffer = new byte[streamSource.Length];
+                        streamSource.Read(buffer, 0, (int)streamSource.Length);
+                        streamDest.Write(buffer , 0 , (int)streamSource.Length);
+                        streamDest.Close();
+                        streamSource.Close();
+                    }
+                }
+                LoadIcon();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("sl_Storage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sl_Storage")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから
+// 見えなくなります。このアセンブリ内で COM から型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+[assembly: Guid("f3e47c1d-a5ef-4c2e-a9e3-85aba26ffc2c")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています。
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// すべての値を指定するか、下のように '*' を使ってリビジョンおよびビルド番号を
+// 既定値にすることができます。
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
